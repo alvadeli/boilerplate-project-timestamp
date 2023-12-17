4d0b16bcebf4fdeef6b07727858cd36bdc3a6087
alvadeli <142695804+alvadeli@users.noreply.github.com>
2023-12-17 17:42:56 +0100

    Implemented timestamp service

--- index.js
+++ index.js
@@ -18,15 +18,47 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  let date = req.params.date
+  const dateFormat = /^\d{4}-\d{2}-\d{2}$/;
+  const unixFomat = /^\d+$/ 
+  let date_unix = 0;
+  let date_yyyymmdd = "";
+
+  let currentDateSet = false;
+  if (!date)
+  {
+    date = new Date().toGMTString();
+    currentDateSet = true;
+  }
+
+  if (dateFormat.test(date) || currentDateSet)
+  {
+    date_yyyymmdd = date;
+    date_unix = Date.parse(date)/1000;
+  } else if (unixFomat.test(date)) {
+    date_unix = parseInt(date);
+    date_yyyymmdd = new Date(parseInt(date_unix)).toGMTString();
+  }
+  else {
+    res.json({error: "Invalid Date"});
+    return;
+  }
+
+  res.json({unix: date_unix, utc: date_yyyymmdd});
+});
+
+
+
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  //console.log('Your app is listening on port ' + listener.address().port);
+  console.log(`Server is running at http://localhost:${listener.address().port}`);
 });
